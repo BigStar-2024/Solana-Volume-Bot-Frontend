58a2e44423a7a2cdf9b270b4dd38d5dc8a9b07c8
BigStar2024 <aadsf7463@gmail.com>
2024-08-22 15:43:58 +0900

    home page first commit

--- src/components/atom/Component2.tsx
+++ src/components/atom/Component2.tsx
@@ -2,25 +2,20 @@ import React from "react"
 
 const Component2 = (
     {
-        src,
-        alt,
         heading,
         description
     }:
         {
-            src: string,
-            alt: string,
             heading: string,
             description: string
         }
 ) => {
     return (
         <div className="flex flex-col items-center border rounded-[9px] border-[#F5B30D] p-12 bg-opacity-30 bg-[#F5B30D] mx-6">
-            <img src={src} alt={alt} className="w-[96px]"></img>
-            <div className="font-spicy text-[36px] mt-6 text-[#F5B30D] text-center">
+            <div className="font-spicy text-[28px] mt-6 text-[#F5B30D] text-center">
                 {heading}
             </div>
-            <div className="font-caveat text-white mt-6 text-[28px] text-center px-4">
+            <div className="font-caveat text-white mt-6 text-[20px] text-center px-4">
                 {description}
             </div>
         </div>
